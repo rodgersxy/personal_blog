d77078ac92eddcb95c71a550f60823e61d47462d
rodgersxy <rodgersny99@gmail.com>
2023-10-26 00:22:42 +0300

    latest update

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# }
+
+
+
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER': 'postgres',
+        'PASSWORD': '28411846',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+    }
 }
 
 
